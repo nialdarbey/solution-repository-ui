584dbacae9efa0c0d1862852c4f509fcf3cc5d79
Nial Darbey <nialdarbey@gmail.com>
2013-07-16 12:01:52 -0300

    sr generic user

--- src/main/app/docroot/js/app.js
+++ src/main/app/docroot/js/app.js
@@ -1,26 +1,11 @@
-AppX = Ember.Application.extend({
+App = Ember.Application.create({
   LOG_TRANSITIONS: true,
-  asrTokenRequestTemplate: 'https://registry-ci.mulesoft.com:443/api/access-token?grant_type=password&client_id=WEBUI&username=USER_NAME&password=PASS_WORD&scope=READ_SERVICES%20WRITE_SERVICES%20 CONSUME_SERVICES%20 APPLY_POLICIES%20READ_CONSUMERS%20WRITE_CONSUMERS%20 CONTRACT_MGMT%20 CONSUME_POLICIES',
-  githubUsername: null,
-  asrToken: localStorage.asrToken,
-  asrTokenChanged: function() {
-    var token = this.get('asrToken');
-    if (token === null) {
-      localStorage.removeItem('asrToken');
-    } else {
-      localStorage.asrToken = this.get('asrToken');
-    }
-  }.observes('asrToken'),
-  githubUsername: localStorage.githubUsername,
-  githubUsernameChanged: function() {
-    localStorage.githubUsername = this.get('githubUsername');
-  }.observes('githubUsername'),
+  asrTokenRequestTemplate: 'https://registry.mulesoft.com:443/api/access-token?grant_type=password&client_id=WEBUI&username=USER_NAME&password=PASS_WORD&scope=READ_SERVICES%20WRITE_SERVICES%20 CONSUME_SERVICES%20 APPLY_POLICIES%20READ_CONSUMERS%20WRITE_CONSUMERS%20 CONTRACT_MGMT%20 CONSUME_POLICIES',
+  drTokenRequestTemplate: 'https://demo-repository-api.cloudhub.io/access-token?grant_type=password&client_id=web-ui&username=USER_NAME&password=PASS_WORD&scope=READ%20WRITE',
+  applicationController: null
 });
 
-App = AppX.create();
-
-
-/*
+/* demo-repository-api.cloudhub.io
 
           API
 
@@ -28,20 +13,25 @@ App = AppX.create();
 
 Api = Milo.API.create();
 
-Api.options('baseUrl', 'https://localhost:9999/api');
+Api.options('baseUrl', 'https://demo-repository-api.cloudhub.io/api');
 
 Api.Demo = Milo.Model.extend({
     rootElement: 'services',
     uriTemplate: '/demos/:id',
-    name: Milo.property('string')
+    name: Milo.property('string'),
+    summary: Milo.property('string'),
+    description: Milo.property('string'),
+    githubOwner: Milo.property('string'),
+    releaseNotes: Milo.property('string'),
+    url: Milo.property('string'),
+    type: Milo.property('string'),
+    version: Milo.property('string')
 });
 Api.Repository = Milo.Model.extend({
     rootElement: 'repositories',
     uriTemplate: '/repositories/:repository_id',
     name: Milo.property('string'),
     readMe: Milo.property('string')
-    //owner: Milo.property("Api.RepositoryOwner"),
-    //createdAt: Milo.property('date')
 });
 Api.RepositoryReadMe = Milo.Model.extend({
     rootElement: 'readMe',
@@ -67,13 +57,15 @@ Api.User = Milo.Model.extend({
     firstName: Milo.property('string'),
     lastName: Milo.property('string'),
     role: Milo.property('string'),
+    srUser: Milo.property('string'),
+    srPass: Milo.property('string'),
     githubUsername: Milo.property('string'),
     fullName: function() {
       var firstName = this.get('firstName');
       var lastName = this.get('lastName');
       return firstName + ' ' + lastName;
     }.property('firstName', 'lastName')
-})
+});
 
 
 
@@ -90,14 +82,21 @@ App.Router.map(function() {
   this.resource('snippets');
   this.resource('gists');
   this.resource('login');
+  this.resource('logout');
+});
+
+App.ApplicationRoute = Ember.Route.extend({
+  setupController: function(controller) {
+    App.set('applicationController',controller);
+  }
 });
 
 App.PrivateRoute = Ember.Route.extend({
 
   redirect: function(controller, context) {
-    if (! App.get('asrToken')) {
+    if (App.applicationController.get('loginExpired')) {
       this.transitionTo('login');
-    }
+    } 
   },
 
   // beforeModel hook is only available from version rc 6
@@ -133,22 +132,21 @@ App.LoginRoute = Ember.Route.extend({
     controller.reset();
   }
 });
-
+App.LogoutRoute = Ember.Route.extend({
+  setupController: function(controller, context) {
+    controller.logout();
+  }
+})
 
 App.RepositoriesRoute = App.PrivateRoute.extend({
   model: function() {
-    return Api.Repository.where({ owner: App.get('githubUsername')}).findMany();
+    return Api.Repository.where({ owner: this.controllerFor('application').get('githubUsername')}).findMany();
   }
 });
-App.RepositoriesIndexRoute = App.PrivateRoute.extend({
-  model: function() {
-    console.log('RepositoriesIndex Route...');
-  }
-});
+
 App.RepositoryRoute = App.PrivateRoute.extend({
   model: function(params) {
-    console.log("Repository Route...  ");
-    return Api.Repository.where({repository_id : params.repository_id, owner: App.get('githubUsername') }).findOne();
+    return Api.Repository.where({repository_id : params.repository_id, owner: this.controllerFor('application').get('githubUsername') }).findOne();
   },
   serialize: function(model, params) {
     return { 
@@ -167,7 +165,7 @@ App.RepositoryRoute = App.PrivateRoute.extend({
 
 App.DemosRoute = App.PrivateRoute.extend({
   model: function() {
-    return Api.Demo.where({ asrToken: App.get('asrToken') }).findMany();
+    return Api.Demo.findMany();
   }
 });
 App.DemoRoute = App.PrivateRoute.extend({
@@ -185,8 +183,66 @@ App.SnippetsRoute = Ember.Route.extend({});
           CONTROLLERS
           */
 
+App.ApplicationController = Ember.Controller.extend({
+  drTokenExpiration: localStorage.drTokenExpiration,
+  drTokenExpirationChanged: function() {
+    localStorage.drTokenExpiration = this.get('drTokenExpiration');
+  }.observes('drTokenExpiration'),
+  drToken: localStorage.drToken,
+  drTokenChanged: function() {
+    var token = this.get('drToken');
+    if (token === null) {
+      localStorage.removeItem('drToken');
+    } else {
+      localStorage.drToken = this.get('drToken');
+    }
+  }.observes('drToken'),
+  drTokenTimestamp: localStorage.drTokenTimestamp,
+  drTokenTimestampChanged: function() {
+    var timestamp = this.get('drTokenTimestamp');
+    localStorage.drTokenTimestamp = timestamp;
+  }.observes('drTokenTimestamp'),
+  asrTokenExpiration: localStorage.asrTokenExpiration,
+  asrTokenExpirationChanged: function() {
+    localStorage.asrTokenExpiration = this.get('asrTokenExpiration');
+  }.observes('asrTokenExpiration'),
+  asrToken: localStorage.asrToken,
+  asrTokenChanged: function() {
+    var token = this.get('asrToken');
+    if (token === null) {
+      localStorage.removeItem('asrToken');
+    } else {
+      localStorage.asrToken = this.get('asrToken');
+    }
+  }.observes('asrToken'),
+  asrTokenTimestamp: localStorage.asrTokenTimestamp,
+  asrTokenTimestampChanged: function() {
+    var timestamp = this.get('asrTokenTimestamp');
+    localStorage.asrTokenTimestamp = timestamp;
+  }.observes('asrTokenTimestamp'),
+  githubUsername: localStorage.githubUsername,
+  githubUsernameChanged: function() {
+    localStorage.githubUsername = this.get('githubUsername');
+  }.observes('githubUsername'),
+  loginExpired: function() {
+    if (this.get('asrToken')) {
+      var now = new Date().getTime();
+      var then = new Date(this.get('asrTokenTimestamp')).getTime();
+      if (now - then >= (this.get('asrTokenExpiration') * 1000)) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  }.property('asrToken','asrTokenTimestamp', 'asrTokenExpiration')
+});
+
 App.LoginController = Ember.Controller.extend({
 
+  needs: ['application'],
+  
   reset: function() {
     this.setProperties({
       username: "",
@@ -194,11 +250,10 @@ App.LoginController = Ember.Controller.extend({
       errorMessage: ""
     });
   },
-
-  /*token: localStorage.token,
-  tokenChanged: function() {
-    localStorage.token = this.get('token');
-  }.observes('token'),*/
+  
+  loggedIn: function() {
+    return ! this.get('controllers.application').get('loginExpired');
+  },
 
   login: function() {
 
@@ -207,25 +262,39 @@ App.LoginController = Ember.Controller.extend({
     // Clear out any error messages.
     this.set('errorMessage', null);
 
-    var asrTokenRequestUrl = App.asrTokenRequestTemplate.replace('USER_NAME', credentials.username).replace('PASS_WORD', credentials.password);
+    var drTokenRequestUrl = App.drTokenRequestTemplate.replace('USER_NAME', credentials.username).replace('PASS_WORD', credentials.password);
 
-    $.getJSON(asrTokenRequestUrl).done(function(tokenDetails) {
-      if (tokenDetails) {
+    var tokenDate = new Date();
+    $.getJSON(drTokenRequestUrl).done(function(drToken) {
+      if (drToken) {
         var attemptedTransition = self.get('attemptedTransition');
         Api.User.where({ id : credentials.username }).findOne()
           .done(function(user) {
-            App.set('asrToken', tokenDetails.access_token);
-            App.set('githubUsername', user.get('githubUsername'));
-            Ember.run.later(this, function(){
-              App.set('asrToken', null);
-            }, (tokenDetails.expires_in - 60) * 1000);
-            if (attemptedTransition) {
-              attemptedTransition.retry();
-              self.set('attemptedTransition', null);
-            } else {
-              // Redirect to 'repositories' by default.
-              self.transitionToRoute('repositories');
-            }
+              var asrTokenRequestUrl = App.asrTokenRequestTemplate.replace('USER_NAME', user.srUser).replace('PASS_WORD', user.srPass);
+              $.getJSON(asrTokenRequestUrl)
+                .done(function(asrToken) {
+                  if (asrToken) {
+                    self.get('controllers.application').set('asrToken', asrToken.access_token);
+                    self.get('controllers.application').set('asrTokenTimestamp', tokenDate);
+                    self.get('controllers.application').set('githubUsername', user.get('githubUsername'));
+                    self.get('controllers.application').set('asrTokenExpiration', asrToken.expires_in);
+                    self.get('controllers.application').set('drToken', drToken.access_token);
+                    self.get('controllers.application').set('drTokenTimestamp', tokenDate);
+                    self.get('controllers.application').set('drTokenExpiration', drToken.expires_in);
+                    Api.queryParams('access_token', drToken.access_token);
+                    Api.queryParams('asrToken', asrToken.access_token);
+                    if (attemptedTransition) {
+                      attemptedTransition.retry();
+                      self.set('attemptedTransition', null);
+                    } else {
+                      // Redirect to 'repositories' by default.
+                      self.transitionToRoute('repositories');
+                    }
+                  }
+                })
+                .fail(function(e) {
+                  
+                })
           })
           .fail(function(e) {
             self.set('errorMessage', 'Sorry, ' + credentials.username + ', but you are not registered yet!');
@@ -237,21 +306,65 @@ App.LoginController = Ember.Controller.extend({
       self.transitionToRoute('login');
     });
   }
+  
+});
+App.LogoutController = Ember.ObjectController.extend({
+  needs: ['application'],
+  logout: function() {
+    this.get('controllers.application').set('asrTokenExpiration', null);
+    this.get('controllers.application').set('asrTokenTimestamp', null);
+    this.get('controllers.application').set('asrToken', null);
+    this.get('controllers.application').set('githubUsername', null);
+    this.get('controllers.application').set('drTokenExpiration', null);
+    this.get('controllers.application').set('drTokenTimestamp', null);
+    this.get('controllers.application').set('drToken', null);
+    this.transitionToRoute('login');
+  }
 });
 App.RepositoryController = Ember.ObjectController.extend({
+    needs: ['application'],
+    
     loadReadMe: function(model) {
-      Api.RepositoryReadMe.where({repository_id : this.get('name'), owner : App.get('githubUsername')}).findOne()
+      Api.RepositoryReadMe.where({access_token: this.get('controllers.application').get('drToken'), repository_id : this.get('name'), owner : App.applicationController.get('githubUsername')}).findOne()
         .done(function(data) {
           var text = data.get('readMe');
-          var newText = text.replace(/[\{\}]/g, '');
-          model.set('readMe', newText);
+          model.set('readMe', text);
         })
         .fail(function(e) {
           model.set('readMe', e[0].statusText);
         });
+    },
+    
+    register: function() {
+      var demo = Api.Demo.create({
+        name: this.get('name'),
+        description: this.get('readMe').substring(0,2048).replace(/\n/g, '\\n'),
+        summary: this.get('description').substring(0,96).replace(/\n/g, '\\n'),
+        githubOwner: this.get('controllers.application').get('githubUsername'),
+        releaseNotes: 'test',
+        url: 'http://localhost',
+        type: 'github',
+        version: '3.4.0'
+      });
+      demo.save().done(function() {
+        alert('registered');
+      });
+      console.log(this.get('name'));
     }
  });
 
+
+/**
+ *         VIEWS
+ */
+
+App.RepositoriesView = Em.View.extend({
+  didInsertElement: function () {
+    $('tbody tr').hover(function(event) {
+      $(this).addClass('highlight').siblings().removeClass('highlight');
+    });
+  }
+});
 
 
 /**
